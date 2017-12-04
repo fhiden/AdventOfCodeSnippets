b631c00fa7e9160b2a66df5b338a1ef4335dc6da
fhiden <fhiden@kth.se>
2017-12-04 20:07:04 +0000

    I missed that there where a second question at first. Now it is added into the Code for day one.

--- FirstDay.java
+++ FirstDay.java
@@ -1,11 +1,15 @@
 class FirstDay{
 	public static void main(String[] args){
 		int sum = 0;
+		int sumb = 0;
 		char[] didgets = args[0].toCharArray();
 		for (int i = 0; i<didgets.length; i++) {
 			if(didgets[i]==didgets[(i+1)%didgets.length])
 				sum += Character.getNumericValue(didgets[i]);
+			if(didgets[i]==didgets[(i+didgets.length/2)%didgets.length])
+				sumb += Character.getNumericValue(didgets[i]);
+
 		}
-		System.out.print("\n\t"+sum+"\n");
+		System.out.print("\n\t"+sum+"\n\n\t"+sumb+"\n");
 	}
 }
